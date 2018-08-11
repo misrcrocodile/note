dc38e933a931a0788fca098f4eeb86795d9e9395
misrcrocodile <misr.crocodile@gmail.com>
2018-08-11 17:42:58 +0900

    change url

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ export const CHANGE_NOTE = 'CHANGE_NOTE';
 export const UPDATE_NOTE = 'UPDATE_NOTE';
 export const UPDATE_NOTE_FULFILLED = 'UPDATE_NOTE_FULFILLED';
 export const UPDATE_NOTE_REJECTED = 'UPDATE_NOTE_REJECTED';
-export const URL = "https://misr-utility-misr-utility.a3c1.starter-us-west-1.openshiftapps.com/note";
+export const URL = "https://utility-utility-server.7e14.starter-us-west-2.openshiftapps.com/note";
 
 export const defaultObj = {
   id: "",
