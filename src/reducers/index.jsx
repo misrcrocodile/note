041fb13082e8181196ea777eb221465da6b5eb1f
misscrocodile <misr.crocodile@gmail.com>
2019-09-16 00:00:36 +0900

    first commit

--- src/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { routerReducer } from "react-router-redux";
-import note from "./noteReducer";
-
-export default combineReducers({
-  note,
-  routing: routerReducer
-});
